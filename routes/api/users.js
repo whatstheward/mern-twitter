70ba8332d7991d86b67b97033fe7316a50eb0eb8
Maddie <whatstheward@gmail.com>
2020-01-15 14:23:27 -0500

    initial commit for frontend

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs')
-const User = require('../../models/User')
 const jwt = require('jsonwebtoken')
 const keys = require('../../config/keys')
 const passport = require('passport')
+
+const User = require('../../models/User')
 
 const validateRegisterInput = require('../../validation/register')
 const validateLoginInput = require('../../validation/login')
