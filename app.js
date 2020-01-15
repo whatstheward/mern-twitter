983948f80e9c4ae3d8fbee8655fcb963dbc22978
Maddie <whatstheward@gmail.com>
2020-01-15 09:30:11 -0500

    add user validations and routes

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-const mongoose = require('mongoose')
-const db = require('./config/keys').mongoURI
-mongoose
-    .connect(db, { useNewUrlParser: true})
-    .then(()=>console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err))
+// Express for routes
 const express = require("express")
 const app = express()
-app.get("/", (req, res)=> res.send("Hello World!"))
+const mongoose = require('mongoose')
+const db = require('./config/keys').mongoURI
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
 
+// Connect to MongoDB
+mongoose
+.connect(db, { useNewUrlParser: true})
+.then(()=>console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err))
+
+// BodyParser to parse JSON
+const bodyParser = require('body-parser')
+// Middleware for BodyParser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+
+// Routes
+app.get("/", (req, res)=> { 
+    res.send("Hello World!")})
+app.use("/api/users", users)
+app.use("/api/tweets", tweets)
+
+    
+    
+// Define port 
 const port = process.env.PORT || 5000
 app.listen(port, ()=> console.log(`Server is running on port ${port}`))
+    
+    
---
+++ routes/api/users.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const bodyParser = require('body-parser')
+
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
+
+router.use(bodyParser.urlencoded({extended:false}))
+router.use(bodyParser.json())
+
+router.post('/register', (req, res) => {
+    debugger
+const { errors, isValid } = validateRegisterInput(req.body);
+
+if (!isValid) {
+    return res.status(400).json(errors);
+}
+
+User.findOne({ email: req.body.email })
+    .then(user => {
+    if (user) {
+        // Use the validations to send the error
+        errors.email = 'Email already exists';
+        return res.status(400).json(errors);
+    } else {
+        const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+        })
+    }
+    })
+})
+
+router.post('/login', (req, res) => {
+const { errors, isValid } = validateLoginInput(req.body);
+
+if (!isValid) {
+    return res.status(400).json(errors);
+}
+
+const email = req.body.email;
+const password = req.body.password;
+
+User.findOne({email})
+    .then(user => {
+    if (!user) {
+        // Use the validations to send the error
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
+    }
+
+    bcrypt.compare(password, user.password)
+        .then(isMatch => {
+        if (isMatch) {
+            res.json({msg: 'Success'});
+        } else {
+            // And here:
+            errors.password = 'Incorrect password'
+            return res.status(400).json(errors);
+        }
+        })
+    })
+})
+
+router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+module.exports = router;
